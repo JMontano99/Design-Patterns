8cd186e7ea611fc34a11bb6cb997b43c034e9a64
JMontano99 <alberto.montano@axity.com>
2024-08-20 22:54:19 +0000

    Modificacion BillingServes,Enrollment,index y notification

--- Creacionales/Singleton/index.ts
+++ Creacionales/Singleton/index.ts
@@ -2,4 +2,9 @@
 import { EnrollmentService } from './EnrollmentService';
 import { BillingService } from './BillingService';
 
+const inscriptionService = new EnrollmentService();
+const billingSer = new  BillingService();
+const contactMail = 'alberto@axity.com';
 
+inscriptionService.enrollStudent(contactMail);
+billingSer.notifyPaymentDue(contactMail);
